214000fcbbd6e64bcc8cfeeeccefdc74daeb168b
emalan <emalan>
2008-03-06 18:22:01 +0000

    dao implementation

--- src/com/madalla/dao/blog/BlogDao.java
+++ src/com/madalla/dao/blog/BlogDao.java
@@ -7,5 +7,8 @@ import com.madalla.service.blog.BlogEntry;
 public interface BlogDao {
 
     public Collection<BlogEntry> getBlogEntriesForSite();
-    public Collection<BlogEntry> getBlogentriesForCategory(String category);
+    public Collection<BlogEntry> getBlogEntriesForCategory(String category);
+    public int insertBlogEntry(BlogEntry blogEntry);
+    public int saveBlogEntry(BlogEntry blogEntry);
+    public void deleteBlogEntry(int blogEntryId);
 }
--- src/com/madalla/dao/blog/JdbcBlogDao.java
+++ src/com/madalla/dao/blog/JdbcBlogDao.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.Collection;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -15,18 +16,38 @@ public class JdbcBlogDao extends NamedParameterJdbcDaoSupport implements BlogDao
     
     private String site ;
     private final static String SQL_SELECT_ALL = 
-        "select category.category_name, entry.entry_date, entry.entry_text from entry, category, site " +
-        "where entry.category_id = category.category_id and " +
-        "entry.site_id = site.site_id and " +
-        "site_name = :site_name";
-    private final static String SQL_SELECT_CATEGORY = "";
-    private final static String SQL_INSERT = "";
-    private final static String SQL_UPDATE = "";
-    private final static String SQL_DELETE = "";
+        "select CATEGORY.CATEGORY_NAME, ENTRY.ENTRY_DATE, ENTRY.ENTRY_TEXT from ENTRY, CATEGORY, SITE " +
+        "where ENTRY.CATEGORY_ID = CATEGORY.CATEGORY_ID and " +
+        "ENTRY.SITE_ID = SITE.SITE_ID and " +
+        "SITE.SITE_NAME = :SITENAME";
+    private final static String SQL_SELECT_CATEGORY = SQL_SELECT_ALL +
+        " and CATEGORY.CATEGORY_NAME = :categoryName";
+    private final static String SQL_INSERT = "insert into ENTRY";
+    private final static String SQL_UPDATE = "update ENTRY";
+    private final static String SQL_DELETE = "delete from ENTRY where ENTRY.ENTRY_ID = ?";
     
     public Collection<BlogEntry> getBlogEntriesForSite(){
-        SqlParameterSource namedParameters = new MapSqlParameterSource("site_name", site);
+        SqlParameterSource namedParameters = new MapSqlParameterSource("siteName", site);
         return getNamedParameterJdbcTemplate().queryForList(SQL_SELECT_ALL, namedParameters);
+    }
+    
+    public Collection<BlogEntry> getBlogEntriesForCategory(String category) {
+        SqlParameterSource parameterSource = new MapSqlParameterSource("siteName", site);
+        return getNamedParameterJdbcTemplate().queryForList(SQL_SELECT_CATEGORY, parameterSource);
+    }
+
+    public int insertBlogEntry(BlogEntry blogEntry) {
+        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(blogEntry);
+        return getNamedParameterJdbcTemplate().update(SQL_INSERT, parameterSource);
+    }
+
+    public int saveBlogEntry(BlogEntry blogEntry) {
+        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(blogEntry);
+        return getNamedParameterJdbcTemplate().update(SQL_UPDATE,parameterSource);
+    }
+
+    public void deleteBlogEntry(int blogEntryId) {
+        getJdbcTemplate().update(SQL_DELETE, new Object[]{blogEntryId});
     }
     
     private static final class BlogRowMapper implements RowMapper{
@@ -40,18 +61,8 @@ public class JdbcBlogDao extends NamedParameterJdbcDaoSupport implements BlogDao
         }   
     }
 
-
-
-
-    public Collection<BlogEntry> getBlogentriesForCategory(String category) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-
-
     public void setSite(String site) {
         this.site = site;
     }
+
 }
