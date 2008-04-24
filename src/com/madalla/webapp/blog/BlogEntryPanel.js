d4d52d243ee76bf06338a94236e81446e07c5b62
emalan <emalan>
2008-04-24 19:44:00 +0000

    fix html editor in IE

--- src/com/madalla/webapp/blog/BlogEntryPanel.js
+++ src/com/madalla/webapp/blog/BlogEntryPanel.js
@@ -13,9 +13,9 @@
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : false,
+		theme_advanced_resizing : false
 
 		// Example content CSS (should be your site CSS)
-		content_css : "css/content.css",
+		//content_css : "css/content.css"
 
 	});
