7d8b2f9de91a093fbe0bb5c9fccbb89d5f941866
emalan <emalan>
2008-09-21 19:55:36 +0000

    email ajax cleanup

--- src/com/madalla/webapp/email/EmailFormPanel.java
+++ src/com/madalla/webapp/email/EmailFormPanel.java
@@ -4,10 +4,10 @@ import java.text.MessageFormat;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.wicket.Component;
 import org.apache.wicket.RequestCycle;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.ajax.form.AjaxFormValidatingBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.behavior.HeaderContributor;
 import org.apache.wicket.markup.ComponentTag;
@@ -18,6 +18,7 @@ import org.apache.wicket.markup.html.form.IFormVisitorParticipant;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.ComponentFeedbackPanel;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.html.resources.CompressedResourceReference;
@@ -49,47 +50,97 @@ public class EmailFormPanel extends Panel {
     private Integer first = CaptchaUtils.randomInteger(1, 20);
     private Integer second = CaptchaUtils.randomInteger(1, 12);
     
-    private String subject;
+    private final ValueMap properties = new ValueMap();
     
-    public EmailFormPanel(String id, String subject) {
+    public EmailFormPanel(final String id, final String subject) {
         super(id);
-        this.subject = subject;
-        Form emailForm = new EmailForm("emailForm", this);
-        //AjaxFormValidatingBehavior.addToAllFormComponents(emailForm,"onblur");
-        add(emailForm);
+        
+        
+        Form form = new EmailForm("emailForm");
+        form.setOutputMarkupId(true);
+
+        final FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
+        feedbackPanel.setOutputMarkupId(true);
+        add(feedbackPanel);
+
+        form.add(new AjaxButton("submit", form){
+        	@Override
+			protected void onSubmit(AjaxRequestTarget target, Form form) {
+				log.debug("onSubmit called - Ajax sending email");
+				target.addComponent(feedbackPanel);
+	            IEmailSender email = getEmailSender();
+	            String body = getEmailBody(properties.getString("name"),properties.getString("email"),properties.getString("comment"));
+	            boolean result = email.sendEmail(subject, body);
+	            if (result){
+	                info("Email sent successfully");
+	            } else {
+	                error("Failed to send email!");
+	            }
+				
+
+			}
+			
+			 @Override
+			 protected void onError(final AjaxRequestTarget target, final Form form) {
+	           	log.debug("onError called");
+	           	target.addComponent(feedbackPanel);
+	           	form.visitFormComponents(new IVisitor() {
+	                public Object formComponent(IFormVisitorParticipant formVisitor) {
+	                    if (formVisitor instanceof FormComponent) {
+	                        FormComponent formComponent = (FormComponent) formVisitor;
+	                        
+	                        if (!formComponent.isValid()){
+	                             target.addComponent(formComponent);
+	                             target.appendJavascript(
+	                                "new Effect.Pulsate($('" + formComponent.getMarkupId() + "'),{pulses:1, duration:0.3});");
+	                             log.debug("Component is invalid. Component MarkupId="+formComponent.getMarkupId()+". Message is " +formComponent.getFeedbackMessage().getMessage());
+	                            
+	                         }
+	                    } else if (formVisitor instanceof ComponentFeedbackPanel){
+	                    	ComponentFeedbackPanel feedback = (ComponentFeedbackPanel) formVisitor;
+	                    	target.addComponent(feedback);
+	                    }
+	                    
+	                    return formVisitor;
+	                }
+	        	});
+
+			 }
+        });
+        add(form);
+        
         add(HeaderContributor.forJavaScript(JS_PROTOTYPE));
         add(HeaderContributor.forJavaScript(JS_EFFECTS));
     }
 
     public class EmailForm extends Form {
         private static final long serialVersionUID = -2684823497770522924L;
-        private final ValueMap properties = new ValueMap();
+        
         
         //private final CaptchaImageResource captchaImageResource;
         
-        public EmailForm(String id, Component panel) {
+        public EmailForm(String id) {
             super(id);
             
             //captchaImageResource = new CaptchaImageResource(imagePass);
-            
-            final FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
-            feedbackPanel.setOutputMarkupId(true);
-            add(feedbackPanel);
-            
+    
             TextField name = new RequiredTextField("name",new PropertyModel(properties,"name"));
             name.add(new ValidationStyleBehaviour());
-            name.setLabel(new Model(panel.getString("label.name")));
+            name.setLabel(new Model(EmailFormPanel.this.getString("label.name")));
+            name.add(new ComponentFeedbackPanel("nameFeedback",name));
             name.add(new AjaxFormComponentUpdatingBehavior("onblur"){
 				private static final long serialVersionUID = -1336792123145056842L;
 				protected void onUpdate(AjaxRequestTarget target) {
 					target.addComponent(getFormComponent());
 				}
             });
+            
             add(name);
             
             RequiredTextField email = new RequiredTextField("email",new PropertyModel(properties,"email"));
             email.add(EmailAddressValidator.getInstance());
             email.add(new ValidationStyleBehaviour());
+            email.add(new ComponentFeedbackPanel("emailFeedback", email));
             email.add(new AjaxFormComponentUpdatingBehavior("onblur"){
 				private static final long serialVersionUID = -6327997848765302354L;
 				protected void onUpdate(AjaxRequestTarget target) {
@@ -134,6 +185,7 @@ public class EmailFormPanel extends Panel {
                 }
             });
             password.add(new ValidationStyleBehaviour());
+            password.add(new ComponentFeedbackPanel("passwordFeedback",password));
             password.add(new AjaxFormComponentUpdatingBehavior("onblur"){
 				private static final long serialVersionUID = 1072604570134494000L;
 
@@ -143,82 +195,41 @@ public class EmailFormPanel extends Panel {
             });
             add(password);
             
-            add(new AjaxButton("submit", this){
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				protected void onSubmit(AjaxRequestTarget target, Form form) {
-					target.addComponent(feedbackPanel);
-				}
-				
-				 @Override
-				 protected void onError(final AjaxRequestTarget target, final Form form) {
-		           	log.debug("onError called");
-		           	target.addComponent(feedbackPanel);
-		           	form.visitFormComponents(new IVisitor() {
-		                public Object formComponent(IFormVisitorParticipant formVisitor) {
-		                    if (formVisitor instanceof FormComponent) {
-		                        FormComponent formComponent = (FormComponent) formVisitor;
-		                        
-		                        if (!formComponent.isValid()){ 
-		                            // If this component failed validation, do something
-		                            // fancy to display it to the user...
-		                             target.addComponent(formComponent);
-		        					 
-		                             // do a little dance
-		                             target.appendJavascript(
-		                                "new Effect.Shake($('" + formComponent.getMarkupId() + "'));");
-		                         }
-		                    }
-		                    return formVisitor;
-		                }
-		        	});
-
-				 }
-            	
-            });
-            
             
         }
-
-		protected void onSubmit() {
-            IEmailSender email = getEmailSender();
-            String body = getEmailBody(properties.getString("name"),properties.getString("email"),properties.getString("comment"));
-            boolean result = email.sendEmail(subject, body);
-            if (result){
-                info("Email sent successfully");
-            } else {
-                error("Failed to send email!");
-            }
-        }
         
-        private String getEmailBody(String from, String email, String comment){
-            Object[] args = {from,email,comment};
-            String body = MessageFormat.format(getEmailtemplate(),args);
-            RequestCycle requestCycle = getRequestCycle();
-            if (requestCycle instanceof WebRequestCycle){
-                WebClientInfo clientInfo = (WebClientInfo)((WebRequestCycle) requestCycle).getClientInfo();
-                ClientProperties cp = clientInfo.getProperties();
 
-                StringBuffer sb = new StringBuffer(body).append(System.getProperty("line.separator"));
-                sb.append("NavigatorAppName : ").append(cp.getNavigatorAppName()).append(System.getProperty("line.separator"));
-                sb.append("NavigatorAppCodeName : ").append(cp.getNavigatorAppCodeName()).append(System.getProperty("line.separator"));
-                sb.append("NavigatorAppVersion : ").append(cp.getNavigatorAppVersion()).append(System.getProperty("line.separator"));
-                sb.append("BrowserVersionMajor : ").append(cp.getBrowserVersionMajor()).append(System.getProperty("line.separator"));
-                sb.append("BrowserVersionMinor : ").append(cp.getBrowserVersionMinor()).append(System.getProperty("line.separator"));
-                body = sb.toString();
-            }
-            return body;
-        }
         
-        private String getEmailtemplate(){
-            StringBuffer sb = new StringBuffer("Email sent from emalan.com website...").append(System.getProperty("line.separator"));
-            sb.append("From: {0} ({1})").append(System.getProperty("line.separator"));
-            sb.append("Comment: {2}").append(System.getProperty("line.separator"));
-            return sb.toString();
-        }
+
 
     }
+    
+    private String getEmailBody(String from, String email, String comment){
+        Object[] args = {from,email,comment};
+        String body = MessageFormat.format(getEmailtemplate(),args);
+        RequestCycle requestCycle = getRequestCycle();
+        if (requestCycle instanceof WebRequestCycle){
+            WebClientInfo clientInfo = (WebClientInfo)((WebRequestCycle) requestCycle).getClientInfo();
+            ClientProperties cp = clientInfo.getProperties();
+
+            StringBuffer sb = new StringBuffer(body).append(System.getProperty("line.separator"));
+            sb.append("NavigatorAppName : ").append(cp.getNavigatorAppName()).append(System.getProperty("line.separator"));
+            sb.append("NavigatorAppCodeName : ").append(cp.getNavigatorAppCodeName()).append(System.getProperty("line.separator"));
+            sb.append("NavigatorAppVersion : ").append(cp.getNavigatorAppVersion()).append(System.getProperty("line.separator"));
+            sb.append("BrowserVersionMajor : ").append(cp.getBrowserVersionMajor()).append(System.getProperty("line.separator"));
+            sb.append("BrowserVersionMinor : ").append(cp.getBrowserVersionMinor()).append(System.getProperty("line.separator"));
+            body = sb.toString();
+        }
+        return body;
+    }
+    
+    private String getEmailtemplate(){
+        StringBuffer sb = new StringBuffer("Email sent from emalan.com website...").append(System.getProperty("line.separator"));
+        sb.append("From: {0} ({1})").append(System.getProperty("line.separator"));
+        sb.append("Comment: {2}").append(System.getProperty("line.separator"));
+        return sb.toString();
+    }
+    
     protected IEmailSender getEmailSender(){
     	return ((IEmailServiceProvider)getApplication()).getEmailSender();
     }
