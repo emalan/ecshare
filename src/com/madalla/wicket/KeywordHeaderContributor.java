74c8dad7b17e26bdb2e5da592ad0d54ec282cbd4
emalan <emalan>
2008-11-19 22:29:42 +0000

    rearrange keywork meta tag

--- src/com/madalla/wicket/KeywordHeaderContributor.java
+++ src/com/madalla/wicket/KeywordHeaderContributor.java
@@ -7,7 +7,7 @@ import org.apache.wicket.behavior.StringHeaderContributor;
 
 public class KeywordHeaderContributor extends StringHeaderContributor {
 	private static final long serialVersionUID = 1L;
-	private static final String contribution = "<meta content=\"{0}\" name=\"KEYWORDS\"/>";
+	private static final String contribution = "<meta name=\"KEYWORDS\" content=\"{0}\"/>";
 	
 	public KeywordHeaderContributor(String keywords) {
 		super(StringUtils.isEmpty(keywords) ? "" : MessageFormat.format(contribution, keywords));
