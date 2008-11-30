08254abd790a8db60e955b65b764795641345aca
emalan <emalan>
2008-11-30 14:30:26 +0000

    register OCM convertors

---
+++ src/com/madalla/util/jcr/ocm/DateTimeConvertor.java
@@ -0,0 +1,7 @@
+package com.madalla.util.jcr.ocm;
+
+import org.apache.jackrabbit.ocm.manager.atomictypeconverter.impl.AtomicTypeConverterProviderImpl;
+
+public class DateTimeConvertor extends AtomicTypeConverterProviderImpl{
+
+}
