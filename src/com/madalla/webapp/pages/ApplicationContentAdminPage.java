f3e5602dcb107d7686e226f350946a799fa3b641
emalan <emalan>
2009-02-02 14:42:07 +0000

    new admin page

---
+++ src/com/madalla/webapp/pages/ApplicationContentAdminPage.java
@@ -0,0 +1,14 @@
+package com.madalla.webapp.pages;
+
+import org.apache.wicket.PageParameters;
+
+import com.madalla.webapp.AdminPage;
+import com.madalla.webapp.ISecureAdminPage;
+import com.madalla.webapp.Panels;
+
+public class ApplicationContentAdminPage extends AdminPage implements ISecureAdminPage{
+
+    public ApplicationContentAdminPage(final PageParameters parameters){
+        add(Panels.contentAdminPanelForAdmin("contentAdminPanel", parameters));
+    }
+}
