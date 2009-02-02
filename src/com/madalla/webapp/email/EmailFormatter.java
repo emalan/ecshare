375f98276b4399cc2e2ff5f5539634c710067384
emalan <emalan>
2009-02-02 16:32:49 +0000

    formatting bug fixed

--- src/com/madalla/webapp/email/EmailFormatter.java
+++ src/com/madalla/webapp/email/EmailFormatter.java
@@ -8,8 +8,8 @@ public class EmailFormatter {
 
 	public static String getUserEmailBody(UserData user, String message) {
 		StringBuffer sb = new StringBuffer("Dear" +
-				(StringUtils.isNotEmpty(" "+user.getFirstName())?user.getFirstName():""+
-				(StringUtils.isNotEmpty(" "+user.getLastName())?user.getLastName():""))+",");
+				(StringUtils.isNotEmpty(user.getFirstName())?" "+user.getFirstName():""+
+				(StringUtils.isNotEmpty(user.getLastName())?" "+user.getLastName():""))+",");
 		sb.append(System.getProperty("line.separator"));
 		sb.append(message);
 		return sb.toString();
