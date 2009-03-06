efe8fd4bef15e5668133998d3aa9de17713ecf58
emalan <emalan>
2009-03-06 17:13:06 +0000

    youtube start

---
+++ src/com/madalla/webapp/google/YoutubePanel.java
@@ -0,0 +1,24 @@
+package com.madalla.webapp.google;
+
+import org.apache.wicket.behavior.HeaderContributor;
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class YoutubePanel extends Panel{
+	
+	private static final long serialVersionUID = 1L;
+	private static final String GSEARCH_CSS = "http://www.google.com/uds/css/gsearch.css";
+	private static final String GSEARCH_SCRIPT = "http://www.google.com/uds/api?file=uds.js&v=1.0";
+	private static final String GSVIDEO_CSS = "http://www.google.com/uds/solutions/videobar/gsvideobar.css";
+	private static final String GSVIDEO_SCRIPT = "http://www.google.com/uds/solutions/videobar/gsvideobar.js";
+	
+	
+	public YoutubePanel(String id) {
+		super(id);
+		//add(HeaderContributor.forJavaScript(GSEARCH_SCRIPT));
+		//add(HeaderContributor.forCss(GSEARCH_CSS));
+		//add(HeaderContributor.forJavaScript(GSVIDEO_SCRIPT));
+		//add(HeaderContributor.forCss(GSVIDEO_CSS));
+		
+	}
+
+}
