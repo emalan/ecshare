d9b217ed7a04a242d0bf0a581a39d196381e8336
emalan <emalan>
2009-03-09 21:11:00 +0000

    style for form

--- src/com/madalla/webapp/google/YoutubePlayerPanel.java
+++ src/com/madalla/webapp/google/YoutubePlayerPanel.java
@@ -10,6 +10,7 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.validation.validator.NumberValidator;
 
 import com.madalla.wicket.configure.AjaxConfigureIcon;
 
@@ -61,7 +62,7 @@ public class YoutubePlayerPanel extends Panel {
 		super(id);
 		videoData = getVideo(id);
 		String url = "http://www.youtube.com/v/"+videoData.getVideoId()+"&enablejsapi=1&playerapiid=ytplayer";
-		add(new SwfObject(url,425, 356));
+		add(new SwfObject(url,videoData.getWidth(), videoData.getHeight()));
 		final AjaxConfigureIcon icon = new AjaxConfigureIcon("configureIcon","formDiv");
 		add(icon);
 		
@@ -72,6 +73,7 @@ public class YoutubePlayerPanel extends Panel {
 
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form form) {
+				target.appendJavascript("cueVideo('"+videoData.getVideoId()+"','"+ videoData.getWidth()+"','"+videoData.getHeight()+"');");
 				target.appendJavascript("cueVideo('"+videoData.getVideoId()+"');");
 				saveVideo();
 				icon.hideConfigureArea(target);
@@ -79,6 +81,8 @@ public class YoutubePlayerPanel extends Panel {
 			
 		});
 		form.add(newFormField("videoid", new PropertyModel(videoData, "videoId")));
+		form.add(newFormField("height", new PropertyModel(videoData, "height")).add(new NumberValidator.RangeValidator(10,700)));
+		form.add(newFormField("width", new PropertyModel(videoData, "width")).add(new NumberValidator.RangeValidator(10,900)));
 		add(form);
 		
 	}
@@ -86,6 +90,7 @@ public class YoutubePlayerPanel extends Panel {
 	private FormComponent newFormField(String componentId, IModel model){
 		final TextField field = new TextField(componentId, model);
 		field.setOutputMarkupId(true);
+		field.setRequired(true);
 		return field;
 	}
 
@@ -93,6 +98,8 @@ public class YoutubePlayerPanel extends Panel {
 	private YtVideo getVideo(String id){
 		YtVideo videoData = new YtVideo();
 		videoData.setVideoId("krb2OdQksMc");
+		videoData.setWidth(425);
+		videoData.setHeight(356);
 		return videoData;
 	}
 	
