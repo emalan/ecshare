4968fd5da9b6e5db09e8594e9164115749651ed5
emalan <emalan>
2009-03-13 20:34:58 +0000

    YUI css

--- resources/com/madalla/webapp/css/Css.java
+++ resources/com/madalla/webapp/css/Css.java
@@ -4,6 +4,8 @@ import org.apache.wicket.behavior.HeaderContributor;
 
 public class Css {
 
+	public static final HeaderContributor YUI_BASE = HeaderContributor.forCss(Css.class,"base.css");
+	public static final HeaderContributor YUI_CORE = HeaderContributor.forCss(Css.class,"reset-fonts-grid.css");
 	public static final HeaderContributor CSS_BUTTONS = HeaderContributor.forCss(Css.class,"button.css");
 	public static final HeaderContributor CSS_IMAGE = HeaderContributor.forCss(Css.class,"image.css");
 	public static final HeaderContributor CSS_FORM = HeaderContributor.forCss(Css.class,"form.css");
