43e316603d9b88cd654209bb9a2e3df1b19c1ddd
emalan <emalan>
2009-05-09 18:09:21 +0000

    show link after login

--- src/com/madalla/webapp/login/aware/LoggedinBookmarkablePageLink.java
+++ src/com/madalla/webapp/login/aware/LoggedinBookmarkablePageLink.java
@@ -43,7 +43,7 @@ public class LoggedinBookmarkablePageLink extends BookmarkablePageLink<Page>{
 
     @Override
     protected boolean callOnBeforeRenderIfNotVisible() {
-        return hide;
+        return true;
     }
 
 }
