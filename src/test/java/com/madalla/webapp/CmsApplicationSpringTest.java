9e6d328d845d913c13547c0bbfb05258172ca1f1
Eugene Malan <eugene@ecmalan.com>
2012-04-28 17:34:19 -0400

    main test

--- src/test/java/com/madalla/webapp/CmsApplicationSpringTest.java
+++ src/test/java/com/madalla/webapp/CmsApplicationSpringTest.java
@@ -3,16 +3,7 @@ package com.madalla.webapp;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.wicket.RuntimeConfigurationType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
-
-import com.madalla.AbstractSpringWicketTester;
-import com.madalla.BuildInformation;
-import com.madalla.email.IEmailSender;
-import com.madalla.service.IDataService;
-import com.madalla.service.IRepositoryAdminService;
 
 /**
  * Spring Integration Test to test Spring start up when using the eccms package.
@@ -22,13 +13,7 @@ import com.madalla.service.IRepositoryAdminService;
  */
 public class CmsApplicationSpringTest extends AbstractDependencyInjectionSpringContextTests{
 
-	private static final Logger log = LoggerFactory.getLogger(CmsApplicationSpringTest.class);
-	
-	private IRepositoryAdminService repositoryAdminService;
-    private IEmailSender emailSender;
-    private IDataService dataService;
-    private BuildInformation buildInformation;
-    private RuntimeConfigurationType configType;
+	private CmsApplication wicketApplication;
 
 	public CmsApplicationSpringTest() {
 		super();
@@ -38,13 +23,19 @@ public class CmsApplicationSpringTest extends AbstractDependencyInjectionSpringC
     @Override
 	protected String[] getConfigLocations() {
     	List<String> configLocations = new ArrayList<String>();
-		configLocations.add("classpath:org/emalan/cms/service/ocm/applicationContext-cms.xml");
+		configLocations.add("classpath:com/madalla/webapp/applicationContext-app.xml");
         configLocations.add("classpath:com/madalla/webapp/applicationContext-test.xml");
         return configLocations.toArray(new String[configLocations.size()]);
     }
 	
 	public void testSpringConfig() {
-		
+		assertNotNull(wicketApplication);
 	}
+
+	public void setWicketApplication(CmsApplication wicketApplication) {
+		this.wicketApplication = wicketApplication;
+	}
+	
+
 
 }
---
+++ src/test/java/com/madalla/webapp/TestCmsApplication.java
@@ -0,0 +1,33 @@
+package com.madalla.webapp;
+
+import java.util.Collection;
+
+import org.apache.wicket.Page;
+
+public class TestCmsApplication extends CmsApplication{
+
+	@Override
+	protected void addAppPages(Collection<Class<? extends Page>> pages) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void addPagesForMenu(Collection<Class<? extends Page>> pages) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isSiteMultilingual() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Class<? extends Page> getHomePage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
