171db66a0b9bbbf69f576c210689b73846f70251
Eugene Malan <eugene@emalan.com>
2012-06-12 08:14:24 -0400

    Spring annotations

--- src/main/java/com/madalla/service/ApplicationServiceImpl.java
+++ src/main/java/com/madalla/service/ApplicationServiceImpl.java
@@ -11,7 +11,6 @@ import org.emalan.cms.bo.email.EmailEntryData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 
 import com.madalla.BuildInformation;
 import com.madalla.bo.member.MemberData;
