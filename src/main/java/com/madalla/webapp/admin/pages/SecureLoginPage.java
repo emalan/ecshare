97cc3954a7b627dc43287d2137754b43911ef297
Eugene Malan <eugene@emalan.com>
2012-06-25 17:05:45 -0400

    user param constructor for secure login page

--- src/main/java/com/madalla/webapp/admin/pages/SecureLoginPage.java
+++ src/main/java/com/madalla/webapp/admin/pages/SecureLoginPage.java
@@ -1,6 +1,9 @@
 package com.madalla.webapp.admin.pages;
 
 import org.apache.wicket.protocol.https.RequireHttps;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.madalla.webapp.admin.AbstractAdminPage;
 import com.madalla.webapp.user.UserLoginPanel;
@@ -9,13 +12,25 @@ import com.madalla.webapp.user.UserLoginPanel;
 public class SecureLoginPage extends AbstractAdminPage{
 
 	private static final long serialVersionUID = 1L;
-
+	private static final Logger log = LoggerFactory.getLogger(SecureLoginPage.class);
+	
     public SecureLoginPage(){
-		this("");
+		init("");
 	}
+    
+    public SecureLoginPage(final PageParameters parameters) {
+        super(parameters);
+        String username = PageUtils.getPageParameter(UserLoginPage.PARAMETERS[0], parameters, "UserLoginPage", "");
+        init(username);
+    }
 
 	public SecureLoginPage(String username) {
-		add(new UserLoginPanel("adminPanel", username));
+	    init(username);
+	}
+	
+	private void init(final String username) {
+	    log.debug("init - username=" + username);
+	    add(new UserLoginPanel("adminPanel", username));
 	}
 
 }
