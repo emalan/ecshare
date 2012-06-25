80c2f21d8a5fb8ed34458de843287a67469ab401
Eugene Malan <eugene@emalan.com>
2012-06-25 15:21:06 -0400

    redirect to secure page using RestartResponseException

--- src/main/java/com/madalla/webapp/admin/pages/UserLoginPage.java
+++ src/main/java/com/madalla/webapp/admin/pages/UserLoginPage.java
@@ -1,6 +1,7 @@
 package com.madalla.webapp.admin.pages;
 
 import org.apache.wicket.RestartResponseAtInterceptPageException;
+import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,7 +47,8 @@ public class UserLoginPage extends AbstractAdminPage{
 	
 	private void redirectToSecurePage(final String username) {
             log.debug("redirectToSecurePage - redirecting to secure page.");
-            throw new RestartResponseAtInterceptPageException(new SecureLoginPage(username));
+            throw new RestartResponseException(SecureLoginPage.class, new PageParameters().set(PARAMETERS[0], username));
+            //throw new RestartResponseAtInterceptPageException(new SecureLoginPage(username));
             //setResponsePage(new SecureLoginPage(username));
 	}
 
